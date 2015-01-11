5980337fb9424962ea9f7bf0273f2192545687a1
Adrian Śliwa <asl@trifork.com>
2015-01-11 20:50:16 +0100

    Adds bot form

--- ieTanks-server/src/main/webapp/js/app.js
+++ ieTanks-server/src/main/webapp/js/app.js
@@ -5,6 +5,7 @@
 var ieTanksApp = angular.module('ieTanksApp', [
     'ieTanksVisualization',
     'ieTanksServices',
+    'botsForm',
     'ngRoute',
     'ngResource'
 ]); // add other modules if dependant here
@@ -19,6 +20,10 @@ ieTanksApp.config(['$routeProvider',
             when('/history', {
                 templateUrl: 'html/history.html',
                 controller: 'GameHistory'
+            }).
+            when('/bots', {
+                templateUrl: 'html/bots.html',
+                controller: 'BotsController'
             }).
             otherwise({
                 redirectTo: '/'
