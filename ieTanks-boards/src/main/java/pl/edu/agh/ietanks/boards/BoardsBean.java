c34e382bc54254460fb8933f9c6f40a00ec3dcc6
Dawid Pazik <dpazik@mobiltek.pl>
2015-01-27 23:46:35 +0100

    Add new default map

--- ieTanks-boards/src/main/java/pl/edu/agh/ietanks/boards/BoardsBean.java
+++ ieTanks-boards/src/main/java/pl/edu/agh/ietanks/boards/BoardsBean.java
@@ -29,19 +29,42 @@ public class BoardsBean implements BoardsReader {
         int width = 10;
         int height = 10;
         List<Field> obstacles = Arrays.asList(
-                new Field(1, 1),
-                new Field(1, 8),
-                new Field(8, 1),
-                new Field(8, 8),
-                new Field(3, 3),
-                new Field(3, 6),
+                new Field(2, 0),
+                new Field(2, 1),
+                new Field(2, 2),
+                new Field(1, 2),
+
+                new Field(0, 5),
+                new Field(0, 6),
+                new Field(1, 6),
+                new Field(2, 6),
+
+                new Field(6, 2),
+                new Field(7, 2),
                 new Field(6, 3),
-                new Field(6, 6)
+                new Field(6, 4),
+                new Field(5, 4),
+                new Field(5, 3),
+                new Field(6, 5),
+
+                new Field(0, 5),
+                new Field(0, 6),
+                new Field(1, 6),
+                new Field(2, 6),
+
+                new Field(5, 7),
+                new Field(6, 7),
+                new Field(7, 7),
+                new Field(7, 8),
+                new Field(7, 9),
+
+                new Field(9, 0),
+                new Field(9, 5)
         );
         List<Field> startingPoints = Arrays.asList(
                 new Field(0, 0),
-                new Field(0, 9),
-                new Field(9, 0),
+                new Field(0, 7),
+                new Field(5, 3),
                 new Field(9, 9)
         );
         return new Board(id, "Simple board", width, height, obstacles, startingPoints);
